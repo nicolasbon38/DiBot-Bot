52c97608aab628216936db37b1bd2b18ccb2d609
Thomas AUJAS <thomas.aujas086@gmail.com>
2021-04-11 23:13:00 +0200

    regarde que fin phrase + enlève ponctuation

--- bot.py
+++ bot.py
@@ -2,16 +2,15 @@ from auth import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRE
 
 import tweepy
 
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
-
+list_di = ['di', 'dhi', 'dy']
+bad_chars = [';', ':', '!', "*", '.']
 compte = 'EmmanuelMacron'
 
 def reply_to_tweet(tweetId, username, content):
     api.update_status('@{} {} !'.format(username, content), in_reply_to_status_id=tweetId)
-
 
 
 def get_last_tweets_from_user(username):
@@ -21,17 +20,17 @@ def get_last_tweets_from_user(username):
 def analyze_tweet(status):
     text = status.full_text
     words = text.split(' ')
-    for word in words:
-        if '@' in words:
-            continue
-        if 'di' in word and 'di' != word[-2:]:
-            content = get_what_to_say(word)
-            print(content)
-            if content:
-                try:
-                    reply_to_tweet(status.id, compte, content)
-                except tweepy.error.TweepError:
-                    pass
+    last_word = words[len(words)-1]
+
+    if 'di' in last_word and 'di' != last_word[-2:]:
+        content = get_what_to_say(''.join(i for i in last_word if not i in bad_chars))
+        if content:
+            try: 
+                print(content)
+                print(text)
+                #reply_to_tweet(status.id, compte, content)
+            except tweepy.error.TweepError:
+                pass
 
 def get_what_to_say(word):
     i = 0
@@ -43,8 +42,6 @@ def get_what_to_say(word):
     if len(word[i + 2:]) <= 1:
         return None
     return word[i + 2:]
-
-
 
 
 def main():
